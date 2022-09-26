390afa0ebebf61364edcf8d6938c6f5d8aa604fa
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-09-26 23:37:44 +0530

    Improve usability of WA Diff bookmarklet

--- whatsapp-diff.js
+++ whatsapp-diff.js
@@ -2,28 +2,34 @@
 
 // #### Usage
 
-// 1. Create a new bookmark in your browser, and add the following code as the
-// URL. (Exclude this comment)
-// 2. Navigate to the first group and click the bookmark.
-// 3. Navigate to the second group and click the bookmark. Open the browser
-// console to see the diff output printed.
+// 1. Navigate to the first group, wait until the member information is
+// populated and click the bookmark.
+
+// 2. Navigate to the second group, wait until the member information is
+// populated and click the bookmark.
 
 javascript: void (function () {
+  const groupInfo = () => {
+    const elements = document.querySelectorAll("#main span[title]");
+    const name = elements[0].textContent;
+    const members = elements[1].textContent.replace(/ /g, "").split(",");
+    return { name, members };
+  };
   const diff = function (a, b) {
     const missing = a.members.filter((name) => !b.members.includes(name));
     const msg = `List of people in ${a.name} but not in ${b.name}`;
     const names = missing.join("\n");
     const count = `${missing.length} people`;
-    console.log(`${msg}\n${names}\n${count}`);
+    alert(`${msg}\n${names}\n${count}`);
   };
-  const elements = document.querySelectorAll("#main span[title]");
-  const name = elements[0].textContent;
-  const members = elements[1].textContent.replace(/ /g, "").split(",");
-  const group = { name, members };
-
+  if (window.location.hostname !== "web.whatsapp.com") {
+    alert("Make sure that the current tab is the WhatsApp Web tab.");
+    return;
+  }
+  const group = groupInfo();
   if (!window.firstGroup) {
     window.firstGroup = group;
-    console.log("Captured info for first group");
+    alert(`Captured info for first group\nName: ${group.name}\nMembers: ${group.members.length}`);
   } else {
     const { firstGroup } = window;
     delete window.firstGroup;
