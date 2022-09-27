984f7e2d3ba1d560134863d9210c461b5746bcc8
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-09-27 08:28:26 +0530

    better-website: Add bookmarklet to make websites readable

---
+++ better-website.js
@@ -0,0 +1,11 @@
+// Use rules from bettermotherfuckingwebsite.com to make websites more readable
+
+javascript: void (function () {
+  var style = document.createElement("style");
+  document.head.appendChild(style);
+  var sheet = style.sheet;
+  sheet.insertRule(
+    "body{ margin:40px auto; max-width:650px; line-height:1.6; font-size:18px; color:#444; padding:0 10px; }"
+  );
+  sheet.insertRule("h1,h2,h3{ line-height:1.2 }");
+})();
